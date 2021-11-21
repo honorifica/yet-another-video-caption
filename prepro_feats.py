a53ec441a28dd956ebf68578f8b14c4f99a63794
mollnn <1084918073@qq.com>
2021-11-21 19:44:48 +0800

    feat: ResNet152特征提取线程级并行

--- prepro_feats.py
+++ prepro_feats.py
@@ -12,6 +12,8 @@ import torch.nn.functional as F
 import pretrainedmodels
 from pretrainedmodels import utils
 
+import threading
+
 C, H, W = 3, 224, 224
 
 
@@ -32,6 +34,28 @@ def extract_frames(video, dst):
                         stdout=ffmpeg_log, stderr=ffmpeg_log)
 
 
+def exone(params, model, load_image_fn, dir_fc, video):
+    video_id = video.split("/")[-1].split(".")[0].split("\\")[-1]
+    dst = params['model'] + '/' + video_id
+    extract_frames(video, dst)
+
+    image_list = sorted(glob.glob(os.path.join(dst, '*.jpg')))
+    samples = np.round(np.linspace(
+        0, len(image_list) - 1, params['n_frame_steps']))
+    image_list = [image_list[int(sample)] for sample in samples]
+    images = torch.zeros((len(image_list), C, H, W))
+    for iImg in range(len(image_list)):
+        img = load_image_fn(image_list[iImg])
+        images[iImg] = img
+    with torch.no_grad():
+        fc_feats = model(images.cuda()).squeeze()
+    img_feats = fc_feats.cpu().numpy()
+    # Save the inception features
+    outfile = os.path.join(dir_fc, video_id + '.npy')
+    np.save(outfile, img_feats)
+    # cleanup
+    shutil.rmtree(dst)
+
 def extract_feats(params, model, load_image_fn):
     global C, H, W
     model.eval()
@@ -41,27 +65,26 @@ def extract_feats(params, model, load_image_fn):
         os.mkdir(dir_fc)
     print("save video feats to %s" % (dir_fc))
     video_list = glob.glob(os.path.join(params['video_path'], '*.avi'))
-    for video in tqdm(video_list):
-        video_id = video.split("/")[-1].split(".")[-1].split("\\")[-1][0]
-        dst = params['model'] + '/' + video_id
-        extract_frames(video, dst)
 
-        image_list = sorted(glob.glob(os.path.join(dst, '*.jpg')))
-        samples = np.round(np.linspace(
-            0, len(image_list) - 1, params['n_frame_steps']))
-        image_list = [image_list[int(sample)] for sample in samples]
-        images = torch.zeros((len(image_list), C, H, W))
-        for iImg in range(len(image_list)):
-            img = load_image_fn(image_list[iImg])
-            images[iImg] = img
-        with torch.no_grad():
-            fc_feats = model(images.cuda()).squeeze()
-        img_feats = fc_feats.cpu().numpy()
-        # Save the inception features
-        outfile = os.path.join(dir_fc, video_id + '.npy')
-        np.save(outfile, img_feats)
-        # cleanup
-        shutil.rmtree(dst)
+    # NP 路并行！！！
+    NP = 1
+    vll = []
+    while len(video_list)>0:
+        vll.append(video_list[:NP])
+        video_list = video_list[NP:]
+
+    print("已启用线程级并行。若出现显存不足，请调小代码中参数 NP")
+
+    for vl in tqdm(vll):
+        thread_pool = []
+        for i in vl:
+            thread_pool.append(threading.Thread(target=exone,args=(params,model,load_image_fn,dir_fc,i)))
+        for i in thread_pool:
+            i.start()
+        for i in thread_pool:
+            i.join()
+
+    print("特征抽抽抽已完成 :)")
 
 
 if __name__ == '__main__':
